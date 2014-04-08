582c6dfb065b6df375d84d616ed70bb27023db8f
patz <jorgedil7@gmail.com>
2014-04-08 00:19:11 -0500

    los paquetes se descargan cuando se usa emacs por primer vez evitando corrupción

--- .emacs.d/elpa/latex-extra-20140325.334/latex-extra-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "latex-extra" "20140325.334" "Adds several useful functionalities to LaTeX-mode." (quote ((auctex "11.86.1"))))
--- .emacs.d/elpa/slamhound-20140318.1352/slamhound-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "slamhound" "20140318.1352" "Rip Clojure namespaces apart and rebuild them." (quote nil))

47926f783c1cf33b63052ce55aaae90f7bcdae46
patz <jorgedil7@gmail.com>
2014-04-08 15:22:55 -0500

    find doc

--- profiles.clj
+++ profiles.clj
@@ -7,7 +7,7 @@
                        [im.chit/vinyasa "0.1.8"]]
         :injections [(require 'vinyasa.inject)
                      (vinyasa.inject/inject 'clojure.core '>
-                       '[[clojure.repl doc source]
+                       '[[clojure.repl doc find-doc source]
                          [clojure.pprint pprint pp]])]
         :aliases {"slamhound" ["run" "-m" "slam.hound"]}
         }

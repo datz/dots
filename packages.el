0eb23f42e9fed7731a6ddfde8cc994c709ae1d40
patz <jorgedil7@gmail.com>
2014-07-04 19:13:05 -0500

    instalación de paquetes necesarios

---
+++ packages.el
@@ -0,0 +1,1 @@
+(defun packages () '(yasnippet yaml-mode solarized-theme slamhound skewer-mode simple-httpd save-packages s rainbow-delimiters popup pkg-info paredit noctilux-theme multiple-cursors magit-filenotify magit latex-extra js2-mode git-rebase-mode git-commit-mode fold-this epl emacs-setup elscreen dash clojure-mode clj-refactor cider auto-complete auctex ac-nrepl ac-math ac-js2))

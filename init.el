0c86eb2fc8a0be6ba5e6658be1eb5fb5e9f01d43
patz <jorgedil7@gmail.com>
2014-07-16 01:52:47 -0500

    fix unmatched ) e instalacion de paquetes sin necesidad de confirmación

--- init.el
+++ init.el
@@ -9,9 +9,7 @@ Return a list of installed packages or nil for every skipped package."
      ;; (package-installed-p 'evil)
      (if (package-installed-p package)
          nil
-       (if (y-or-n-p (format "Package %s is missing. Install it? " package))
-           (package-install package)
-         package)))
+       (package-install package)))
    packages))
 
 
@@ -21,4 +19,4 @@ Return a list of installed packages or nil for every skipped package."
     (package-refresh-contents))
 
 (apply 'ensure-package-installed
-       (packages)))
+       (packages))

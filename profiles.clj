763de70dfc5c0057f07a42046352765b25b9fcf1
patz <jorgedil7@gmail.com>
2015-06-17 20:20:02 -0500

    nueva version de nrepl

--- profiles.clj
+++ profiles.clj
@@ -3,8 +3,8 @@
         :plugins [[lein-kibit "0.0.8"]
                                   [jonase/eastwood "0.1.4"]
                                   [cider/cider-nrepl "0.9.0-SNAPSHOT"]
-                                  ]
-        :dependencies [[org.clojure/tools.nrepl "0.2.7"]
+                                  [refactor-nrepl "1.0.5"]]
+        :dependencies [[org.clojure/tools.nrepl "0.2.10"]
                        [slamhound "1.5.5"]
                        [im.chit/vinyasa "0.1.8"]]
         :injections [(require 'vinyasa.inject)

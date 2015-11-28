070f9bef4700d5b78cf3dbc56f427b327afaea96
patz <jorgedil7@gmail.com>
2015-11-28 17:49:23 -0600

    ident

--- profiles.clj
+++ profiles.clj
@@ -1,9 +1,9 @@
 {
  :user {:global-vars {*print-length* 100}
         :plugins [[lein-kibit "0.0.8"]
-                                  [jonase/eastwood "0.1.4"]
-                                  [cider/cider-nrepl "0.10.0-SNAPSHOT"]
-                                  [refactor-nrepl "1.2.0-SNAPSHOT"]]
+                  [jonase/eastwood "0.1.4"]
+                  [cider/cider-nrepl "0.10.0-SNAPSHOT"]
+                  [refactor-nrepl "1.2.0-SNAPSHOT"]]
         :dependencies [[org.clojure/tools.nrepl "0.2.10"]
                        [slamhound "1.5.5"]
                        [im.chit/vinyasa "0.1.8"]

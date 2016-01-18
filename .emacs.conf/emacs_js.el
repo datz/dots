defabb64a47579387f817cde3477ca14ea1f0e87
patz <jorgedil7@gmail.com>
2016-01-18 00:07:45 -0600

    bordes :D

--- .emacs.conf/emacs_js.el
+++ .emacs.conf/emacs_js.el
@@ -1,3 +1,8 @@
 (add-hook 'js-mode-hook 'js2-minor-mode)
 (add-hook 'js2-mode-hook 'ac-js2-mode)
 (add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
+(setq js2-highlight-level 3)
+(add-hook 'js-mode-hook 'my-paredit-nonlisp) ;use with the above function
+(add-hook 'js-mode-hook 'esk-paredit-nonlisp) ;for emacs starter kit
+(define-key js-mode-map "{" 'paredit-open-curly)
+(define-key js-mode-map "}" 'paredit-close-curly-and-newline)

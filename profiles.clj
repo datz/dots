3d325e155789ab67ebd3313b4eded6155e145ce8
patz <jorgedil7@gmail.com>
2017-03-14 23:57:27 -0600

    version de nrepl

--- profiles.clj
+++ profiles.clj
@@ -2,7 +2,7 @@
  :user {:global-vars {*print-length* 100}
         :plugins [[lein-kibit "0.0.8"]
                   [jonase/eastwood "0.1.4"]
-                  [cider/cider-nrepl "0.10.0-SNAPSHOT"]
+                  [cider/cider-nrepl "0.11.0-SNAPSHOT"]
                   [refactor-nrepl "2.0.0-SNAPSHOT"]]
         :dependencies [[org.clojure/clojure "1.7.0"]
                        [org.clojure/tools.nrepl "0.2.12"]

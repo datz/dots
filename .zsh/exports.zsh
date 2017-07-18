7eecff4e5bdeb1dee6634064ebf9e4bc3a506937
jorge diaz <jorgedil7@gmail.com>
2017-07-18 18:44:17 -0500

    exports

--- .zsh/exports.zsh
+++ .zsh/exports.zsh
@@ -2,6 +2,7 @@ export HISTFILE=~/.zsh_history
 export HISTSIZE=1000
 export SAVEHIST=$HISTSIZE
 export EDITOR=vim
-export PATH=$HOME/bin/:$PATH
 export BROWSER="google-chrome"
 
+export GOPATH=~/src/go
+export PATH="$PATH:$HOME/bin:$GOPATH/bin"

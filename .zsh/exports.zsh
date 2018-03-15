831ebe9ec96347de87c70853326fb3411f0e2c31
Jorge Díaz <jorgedil7@gmail.com>
2018-03-15 10:41:59 -0600

    less with colors

--- .zsh/exports.zsh
+++ .zsh/exports.zsh
@@ -1,8 +1,21 @@
 export HISTFILE=~/.zsh_history
 export HISTSIZE=1000
 export SAVEHIST=$HISTSIZE
-export EDITOR=emacs
 export PATH=$PATH:$HOME/opt/node/bin
 export BROWSER="google-chrome"
 export GOPATH=~/src/go
 export PATH="$PATH:$HOME/bin:$GOPATH/bin:$HOME/opt/node/bin"
+
+
+#man
+
+export MANROFFOPT='-c'
+export LESS_TERMCAP_mb=$(tput bold; tput setaf 2)
+export LESS_TERMCAP_md=$(tput bold; tput setaf 6)
+export LESS_TERMCAP_me=$(tput sgr0)
+export LESS_TERMCAP_so=$(tput bold; tput setaf 3; tput setab 4)
+export LESS_TERMCAP_se=$(tput rmso; tput sgr0)
+export LESS_TERMCAP_us=$(tput smul; tput bold; tput setaf 7)
+export LESS_TERMCAP_ue=$(tput rmul; tput sgr0)
+export LESS_TERMCAP_mr=$(tput rev)
+export LESS_TERMCAP_mh=$(tput dim)
